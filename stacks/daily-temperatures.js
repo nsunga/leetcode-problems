a67381699babef6710358dea77808134a0b92d3c
nsunga <nick.sunga95@gmail.com>
2024-07-11 15:58:37 -0700

    GH-000: did daily temperatures

---
+++ stacks/daily-temperatures.js
@@ -0,0 +1,43 @@
+// 739. Daily Temperatures
+
+// Given an array of integers temperatures represents the daily temperatures, return an array answer such that answer[i] is the number of days you have to wait after the ith day to get a warmer temperature. If there is no future day for which this is possible, keep answer[i] == 0 instead.
+
+// Example 1:
+
+// Input: temperatures = [73,74,75,71,69,72,76,73]
+// Output: [1,1,4,2,1,1,0,0]
+// Example 2:
+
+// Input: temperatures = [30,40,50,60]
+// Output: [1,1,1,0]
+// Example 3:
+
+// Input: temperatures = [30,60,90]
+// Output: [1,1,0]
+
+// Constraints:
+
+// 1 <= temperatures.length <= 105
+// 30 <= temperatures[i] <= 100
+
+/**
+ * @param {number[]} temperatures
+ * @return {number[]}
+ */
+var dailyTemperatures = function(temperatures) {
+    const result = new Array(temperatures.length).fill(0);
+    const stack = [];
+
+    for (const [index, temperature] of temperatures.entries()) {
+        while (stack.length && temperature > stack[stack.length - 1].temperature) {
+            const coolerTemperature = stack.pop();
+            const daysToFindWarmerTemperature = index - coolerTemperature.index;
+
+            result[coolerTemperature.index] = daysToFindWarmerTemperature;
+        }
+
+        stack.push({ index, temperature });
+    }
+
+    return result;
+};
