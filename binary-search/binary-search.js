13a51a694c0cc2fc111a76082b5afc3aa981e47b
nsunga <nick.sunga95@gmail.com>
2024-07-16 21:05:10 -0700

    GH-000: started binary search roadmap and updated the old one

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -1,4 +1,5 @@
 // 704. Binary Search
+
 // Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
 
 // You must write an algorithm with O(log n) runtime complexity.
@@ -13,7 +14,7 @@
 // Input: nums = [-1,0,3,5,9,12], target = 2
 // Output: -1
 // Explanation: 2 does not exist in nums so return -1
- 
+
 // Constraints:
 
 // 1 <= nums.length <= 104
@@ -36,14 +37,14 @@ var search = function(nums, target) {
         // (2lp + rp - lp)/2
         // (2lp)/2 + (rp - lp)/2
         // lp + (rp - lp)/2 - this avoids overflow
-        const midpoint = Math.floor((rightPointer + leftPointer)/2);
+        const midPointer = leftPointer + Math.floor((rightPointer - leftPointer)/2);
 
-        if (nums[midpoint] < target) {
-            leftPointer = midpoint + 1;
-        } else if (nums[midpoint] > target) {
-            rightPointer = midpoint - 1;
-        } else if (nums[midpoint] === target) {
-            return midpoint;
+        if (nums[midPointer] < target) {
+            leftPointer = midPointer + 1;
+        } else if (nums[midPointer] > target) {
+            rightPointer = midPointer - 1;
+        } else {
+            return midPointer;
         }
     }
 
@@ -51,21 +52,21 @@ var search = function(nums, target) {
 };
 
 var searchRecursively = function(nums, target) {
-    const binarySearch = function(nums, target, leftPointer, rightPointer) {
+    const binarySearch = function(leftPointer, rightPointer) {
         if (leftPointer > rightPointer) {
-            return - 1;
+            return -1;
         }
 
-        const midpoint = Math.floor((leftPointer + rightPointer)/2);
+        const midPointer = leftPointer + Math.floor((rightPointer - leftPointer)/2);
 
-        if (nums[midpoint] < target) {
-            return binarySearch(nums, target, midpoint + 1, rightPointer);
-        } else if (nums[midpoint] > target) {
-            return binarySearch(nums, target, leftPointer, midpoint - 1);
-        } else if (nums[midpoint] === target) {
-            return midpoint;
+        if (nums[midPointer] > target) {
+            return binarySearch(leftPointer, midPointer - 1);
+        } else if (nums[midPointer] < target) {
+            return binarySearch(midPointer + 1, rightPointer);
+        } else {
+            return midPointer;
         }
     }
 
-    return binarySearch(nums, target, 0, nums.length - 1);
+    return binarySearch(0, nums.length - 1);
 };
